5aa3c2b00ed41e8252c96b08476d36e93acb3e18
falguni077 <agirlcode07@gmail.com>
2025-11-13 15:44:51 +0530

    made some changes in the launcher cpp and modifyied core.ps1

--- src/launcher.cpp
+++ src/launcher.cpp
@@ -1,24 +1,46 @@
 #include <iostream>
 #include <string>
-#include <windows.h>
-
-// Forward declaration
-
 #include <sstream>
-// ...existing code...
-std::string getInstallPath() {
+#include <cstdlib>
+
+#ifdef _WIN32
+#include <windows.h>
+#else
+#include <limits.h>
+#include <unistd.h>
+#endif
+
+std::string getInstallPath(const char *argv0) {
+#ifdef _WIN32
     char path[MAX_PATH];
     GetModuleFileNameA(NULL, path, MAX_PATH);
     std::string fullPath(path);
     size_t pos = fullPath.find_last_of("\\/");
     return (pos == std::string::npos) ? fullPath : fullPath.substr(0, pos);
+#else
+    char resolved[PATH_MAX];
+    if (realpath(argv0, resolved)) {
+        std::string fullPath(resolved);
+        size_t pos = fullPath.find_last_of('/');
+        return (pos == std::string::npos) ? fullPath : fullPath.substr(0, pos);
+    }
+    return ".";
+#endif
 }
-void executeCommand(const std::string &command)
+
+void executeCommand(const std::string &command, const std::string &installPath)
 {
-    std::string scriptPath = getInstallPath() + "\\..\\scripts\\win\\core.ps1";
-    // Quote the script path
+#ifdef _WIN32
+    std::string scriptPath = installPath + "\\..\\scripts\\win\\core.ps1";
     std::string fullCommand = "powershell -ExecutionPolicy Bypass -File \"" + scriptPath + "\" " + command;
     system(fullCommand.c_str());
+#else
+    // On macOS/Linux the main logic lives in scripts/mac/core.sh
+    std::string scriptPath = installPath + "/../scripts/mac/core.sh";
+    // Ensure the script is executed with bash
+    std::string fullCommand = "/bin/bash \"" + scriptPath + "\" " + command;
+    system(fullCommand.c_str());
+#endif
 }
 
 int main(int argc, char *argv[])
@@ -32,8 +54,8 @@ int main(int argc, char *argv[])
     std::ostringstream command;
     for (int i = 1; i < argc; ++i)
     {
-        // Quote each argument if it contains spaces
         std::string arg = argv[i];
+        // Quote each argument if it contains spaces
         if (arg.find(' ') != std::string::npos)
             command << "\"" << arg << "\"";
         else
@@ -42,6 +64,7 @@ int main(int argc, char *argv[])
             command << " ";
     }
 
-    executeCommand(command.str());
+    std::string installPath = getInstallPath(argv[0]);
+    executeCommand(command.str(), installPath);
     return 0;
 }
